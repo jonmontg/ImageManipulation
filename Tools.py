2b38ab81da1571b0ba5b1736efcedaefc6851b09
Jonathan Montgomery <Montgomery_Jonathan_M@cat.com>
2019-03-23 13:48:58 -0400

    edge detection

--- Tools.py
+++ Tools.py
@@ -1,5 +1,6 @@
 import numpy as np
 import array
+from scipy.ndimage import gaussian_filter
 
 
 #  Returns a 1536 (horizontal) by 1024 (vertical) matrix of linearly scaled pixel intensities. The linear scaling
@@ -13,3 +14,8 @@ def get_matrix(filepath):
     mat = np.array(arr, dtype=np.float16).reshape(1024, 1536)
     return np.divide(mat, np.amax(mat))
 
+
+#  Returns a 2d numpy array of pixel intensities derived from the given matrix. This edge detector finds the difference
+#  between an image and its copy with an applied gaussian filter
+def get_edges(image_matrix):
+    return np.subtract(gaussian_filter(image_matrix), image_matrix)
